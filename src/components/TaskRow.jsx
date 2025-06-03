21f02c7043632eabba61c3f889a45e4350867d54
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-03 16:28:02 +0200

    [4-5][milestone 3 completed]

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,11 +1,16 @@
-const TaskRow = function ({ task }) {
+import { memo } from 'react';
+
+const TaskRow = memo(({ task }) => {
+
+    const statusCLassName = task.status.replace(" ", "").toLowerCase();
+
     return (
         <tr>
             <td>{task.title}</td>
-            <td>{task.status}</td>
+            <td className={statusCLassName}>{task.status}</td>
             <td>{new Date(task.createdAt).toLocaleDateString()}</td>
         </tr>
     )
-}
+})
 
 export default TaskRow;
