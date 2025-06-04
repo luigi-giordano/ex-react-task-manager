fa5582a98b04eb7b7d1ee249d1fb0a83b10d5390
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-04 11:23:23 +0200

    [Link to task][milestone 7]

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 const TaskRow = memo(({ task }) => {
 
@@ -6,7 +7,7 @@ const TaskRow = memo(({ task }) => {
 
     return (
         <tr>
-            <td>{task.title}</td>
+            <td><Link to={`/task/${task.id}`}>{task.title}</Link></td>
             <td className={statusCLassName}>{task.status}</td>
             <td>{new Date(task.createdAt).toLocaleDateString()}</td>
         </tr>
