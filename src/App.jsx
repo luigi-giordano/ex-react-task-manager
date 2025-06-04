5bf3f592039c02d2daf3a016a4838dbbc1459a80
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-04 11:26:15 +0200

    [route to TaskDetail in App][milestone 7]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import TaskList from './pages/TaskList';
 import AddTask from './pages/AddTask';
+import TaskDetail from './pages/TaskDetail';
 import { GlobalProvider } from './context/GlobalContext';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<TaskList />} />
           <Route path="/addTask" element={<AddTask />} />
+          <Route path="/task/:id" element={<TaskDetail />} />
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
